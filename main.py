d91858a8c4daa786c84c8004a30b6eaf5bec43c4
igor <igoreira0@gmail.com>
2020-06-19 13:48:07 -0300

    desafio 2 parte 1 finalizado

--- main.py
+++ main.py
@@ -8,4 +8,19 @@ def desafio1(n):
     return aux
 
 
-print(desafio1(6))
+def desafio2(n):
+    if n <= 25:
+        return "intervalo [0..25]"
+    elif n <= 50:
+        return f"intervalo [25..50]"
+    elif n <= 75:
+        return "intervalo [50..75]"
+    elif n <= 100:
+        return "intervalo [75..100]"
+    else:
+        return "fora de intervalo"
+
+
+print("desafio 1 = ", desafio1(6))
+print("desafio 2 = ", desafio2(25))
+
